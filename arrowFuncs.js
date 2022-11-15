80147bfa8de0ea9882a675f9cf9150e7baff3dae
Amber Cui <97977446+AmberLCui@users.noreply.github.com>
2022-11-15 09:34:23 -0500

    Add files via upload

---
+++ arrowFuncs.js
@@ -0,0 +1,5 @@
+//ES2015 Arrow Functions Short hand
+const double = arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions
+const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0);
